b22f2ff6b590698015549d06df817719f4b7e243
Brian Luft <brian@electroly.com>
2025-01-05 00:53:50 +0000

    Add eslint

---
+++ src/eslint.config.js
@@ -0,0 +1,27 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import globals from 'globals';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      },
+      globals: {
+        ...globals.browser
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      ...tseslint.configs['recommended'].rules,
+      '@typescript-eslint/no-unused-vars': 'error'
+    }
+  }
+]; 
